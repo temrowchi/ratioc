adacd81f3836352830bbab2f2eaee4a9656e4908
Temilola Aderibigbe <temz4rill@gmail.com>
2023-03-12 22:09:09 -0400

    implemented dynamic chat action and search functionality

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -2,52 +2,54 @@ import React, {Component, useRef, useState} from 'react';
 import {Button, FormControl, InputGroup, Col, FormGroup} from 'react-bootstrap';
 import {MessageList, Navbar as NavbarComponent, Avatar} from 'react-chat-elements';
 import {userMessage} from '../mocks';
+import {User} from '../model';
 
-const ChatBox = ({targetUser, onBackPressed, signedInUser}: any) => {
+interface IChatBox {
+  targetUser?: User;
+}
+
+const ChatBox = ({targetUser}: IChatBox) => {
   const [messageText, setMessageText] = useState('');
   const messageListRef = useRef();
-  return (
+
+  return targetUser ? (
     <div>
       <NavbarComponent
         left={
           <div>
-            <Avatar
-              src={'https://avatars.githubusercontent.com/u/80540635?v=4'}
-              alt={'logo'}
-              size="large"
-              type="circle flexible"
-            />
-            <p className="navBarText">{'targetUser?.name'}</p>
+            <Avatar src={targetUser?.avatar} alt={'logo'} size={'large'} type={'circle flexible'} />
+            <p className={'navBarText'}>{targetUser?.name}</p>
           </div>
         }
       />
       <MessageList
         referance={messageListRef}
-        className="messages"
+        className={'messages'}
         lockable={true}
         toBottomHeight={'100%'}
-        dataSource={userMessage}
-        // dataSource={targetUser.messages}
+        dataSource={targetUser.messages}
       />
       <FormGroup>
         <InputGroup>
           <FormControl
-            type="text"
+            type={'text'}
             value={messageText}
             onChange={(e) => {
               setMessageText(e.target.value);
             }}
-            placeholder="Type a message here"
-            key="messageTextBox"
-            className="messageTextBox"
+            placeholder={'Type a message here'}
+            key={'messageTextBox'}
+            className={'messageTextBox'}
             autoFocus
           />
-          <Button disabled={!messageText} className="sendButton" onClick={() => {}}>
-            Send
+          <Button disabled={!messageText} className={'sendButton'} onClick={() => {}}>
+            {'Send'}
           </Button>
         </InputGroup>
       </FormGroup>
     </div>
+  ) : (
+    <></>
   );
 };
 
--- client/src/pages/Chat/index.tsx
+++ client/src/pages/Chat/index.tsx
@@ -2,34 +2,38 @@ import React, {useState} from 'react';
 import {ChatBox, NavBar, UserList} from '../../components';
 import {Container, Row, Col} from 'react-bootstrap';
 import {userList} from '../../mocks';
+import {User} from '../../model';
+// https://github.com/ShankyTiwari/Real-time-private-chatting-app-using-React-Nodejs-mongodb-and-Socket.io
 
 const Chat = () => {
   const [users, setUsers] = useState(userList);
   const [showList, setShowList] = useState(true);
   const [showChatBox, setShowChatBox] = useState(true);
-  const [userChatData, setUserChatData] = useState([]);
+  const [userChatData, setUserChatData] = useState<User | undefined>();
 
-  const onChatClicked = () => {};
+  const onChatClicked = (user: User) => {
+    for (let index = 0; index < users.length; index++) {
+      if (users[index].userId === user.userId) {
+        users[index].unread = 0;
+        setUserChatData(users[index]);
+      }
+    }
+  };
 
   return (
-    <div>
+    <>
       <NavBar signedInUser={'this.state.user'} />
       <Container fluid>
         <Row className="show-grid" style={{marginTop: '20px'}}>
           <Col md={4}>
-            <UserList users={[...userList, ...userList, ...userList]} onChatClicked={onChatClicked} />
+            <UserList users={users} onChatClicked={onChatClicked} />
           </Col>
           <Col md={8}>
-            <ChatBox
-              signedInUser={'this.state.user'}
-              onSendClicked={'this.createMessage.bind(this)'}
-              onBackPressed={'this.toggleViews.bind(this)'}
-              targetUser={'this.state.userChatData[this.state.selectedUserIndex]'}
-            />
+            <ChatBox targetUser={userChatData} />
           </Col>
         </Row>
       </Container>
-    </div>
+    </>
   );
 };
 export default Chat;
